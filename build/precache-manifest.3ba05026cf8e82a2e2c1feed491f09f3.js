c352eaa5ca713bd6f6b53f04f29aecefd76ece36
Yan <yanvancelis@gmail.com>
2020-08-10 13:57:32 -0300

    build 1

---
+++ build/precache-manifest.3ba05026cf8e82a2e2c1feed491f09f3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1224fd6afe70df7315a86de80c69d47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6aface96163541cd4224",
+    "url": "/static/css/main.2f3b442f.chunk.css"
+  },
+  {
+    "revision": "7522fbcd817f6d2705d8",
+    "url": "/static/js/2.276212f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.276212f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aface96163541cd4224",
+    "url": "/static/js/main.193cb1c9.chunk.js"
+  },
+  {
+    "revision": "51db1d082b1c21d9cf38",
+    "url": "/static/js/runtime-main.cda13e31.js"
+  },
+  {
+    "revision": "9e4f43c94bb54b149b0359e1acc382fa",
+    "url": "/static/media/bp.9e4f43c9.svg"
+  },
+  {
+    "revision": "135fa24875c8ec1999a08da3afb2cd45",
+    "url": "/static/media/jp.135fa248.png"
+  },
+  {
+    "revision": "9e4f43c94bb54b149b0359e1acc382fa",
+    "url": "/static/media/logo.9e4f43c9.svg"
+  },
+  {
+    "revision": "47900f8f6522f0126b77ef3a03b9bf2c",
+    "url": "/static/media/yan.47900f8f.jpg"
+  }
+]);
